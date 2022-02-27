ec008a748f2896dbd84d60993d373d8464b14270
qwer <85132981+ch-tuna@users.noreply.github.com>
2022-02-28 07:27:21 +0900

    camera and audio

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,69 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasList = document.getElementById("cameras");
+
+let mystream;
+let muted = false;
+let cameraOff = false;
+
+async function getCamera(){
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        console.log(cameras);
+        cameras.forEach((cam) => {
+            const option = document.createElement("option");
+            option.value = cam.deviceId;
+            option.innerText = cam.label;
+            camerasList.appendChild(option);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getMedia(){
+    try{
+        mystream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        })
+        myFace.srcObject = mystream;
+        await getCamera();
+    }catch(e){
+        console.log(e);
+    }
+}
+
+getMedia();
+
+function handleMuteButn(){
+    mystream.getAudioTracks().forEach(track => {
+        track.enabled = !track.enabled;
+    });
+    if(!muted){
+        muteBtn.innerText = "unmute";
+        muted = true;
+    }else{
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+function handleCameraButn(){
+    mystream.getVideoTracks().forEach(track => {
+        track.enabled = !track.enabled;
+    });
+    if(!muted){
+        cameraBtn.innerText = "CameraOn";
+        cameraOff = true;
+    }else{
+        cameraBtn.innerText = "CameraOff";
+        cameraOff = false;
+    }
+}
+
+muteBtn.addEventListener("click", handleMuteButn);
+cameraBtn.addEventListener("click", handleCameraButn);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,6 @@
 import http from "http";
+import SocketIO from "socket.io";
 import express from "express";
-import {Server, Socket} from 'socket.io';
-const { instrument } = require("@socket.io/admin-ui");
-import { publicDecrypt } from "crypto";
-
-const app = express();
 
 const app = express();
 app.set("view engine", "pug");
@@ -14,7 +10,6 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-
 const wsServer = SocketIO(httpServer);
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
